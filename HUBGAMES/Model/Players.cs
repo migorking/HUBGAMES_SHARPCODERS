4c13d60193c868193a6d52a2269d08304e4d9c81
Mario Igor Barbosa <84343722+migorking@users.noreply.github.com>
2023-01-28 00:32:20 -0300

    Add Register, login e delete usuario

--- HUBGAMES/Model/Players.cs
+++ HUBGAMES/Model/Players.cs
@@ -6,6 +6,11 @@ namespace HUBGAMES.Model
 {
     internal class Players : HubDeGames
     {
+        public Players()
+        {
+            PlayerList = new List<Player>();
+            Recarregar();
+        }
 
         public void RegisterUser(string nick, string password, string email)
         {
@@ -29,17 +34,7 @@ namespace HUBGAMES.Model
 
         }
 
-        public Players()
-        {
-            PlayerList = new List<Player>();
-            Recarregar();
-        }
-
-
-
-
-
-        public bool Login(string password, string email)
+        public bool Login(string password, string email) // faz o login do usuario
         {
             Console.WriteLine("INSIRA SEU EMAIL: ");
             string emailLogin = Console.ReadLine();
@@ -49,14 +44,46 @@ namespace HUBGAMES.Model
             Player user = PlayerList.FirstOrDefault(e => e.Email == emailLogin);
             if (user != null && user.Password == passwordLogin)
             {
+                Console.WriteLine("Usuario Logado com Sucesso");
                 return true;
             }
+            Console.WriteLine("Falha no login");
             return false;
         }
 
-        public void DeleteUser()
+        public void DeleteUser(string nick, string email, string password)
         {
+            string filePath = @"E:\SHARPCODERS\PROJETOS SHARPCODERS\HUBGAMES\Data\user.json";
+            Console.WriteLine("INSIRA SEU USUARIO: ");
+            string usuarioDelete = Console.ReadLine();
+            Console.WriteLine("INSIRA SEU EMAIL: ");
+            string emailDelete = Console.ReadLine();
+            Console.WriteLine("INSIRA SEU PASSWORD: ");
+            string passwordDelete = Console.ReadLine();
 
+            
+            string jsonString = File.ReadAllText(filePath);
+            jsonString.Trim();
+            List<Player> players = JsonConvert.DeserializeObject<List<Player>>(jsonString);
+
+            players.RemoveAll(p => p.Email == emailDelete); //escolher o item mais unico para fazer a remoção.
+            jsonString = JsonConvert.SerializeObject(players);
+            File.WriteAllText(filePath, jsonString);
+
+
+        }
+
+
+        public void ListarTodos()
+        {
+            string filePath = @"E:\SHARPCODERS\PROJETOS SHARPCODERS\HUBGAMES\Data\user.json";
+            string jsonString = File.ReadAllText(filePath);
+            jsonString.Trim();
+            List<Player> players = JsonConvert.DeserializeObject<List<Player>>(jsonString);
+            foreach (var item in players)
+            {
+                Console.WriteLine(item);
+            }
         }
 
 
@@ -65,13 +92,18 @@ namespace HUBGAMES.Model
             string filePath = @"E:\SHARPCODERS\PROJETOS SHARPCODERS\HUBGAMES\Data\user.json";
             string jsonSalvar = JsonConvert.SerializeObject(PlayerList);
             File.WriteAllText(filePath, jsonSalvar);
+            
+            
         }
         private void Recarregar()
         {
-            if (File.Exists("user.json"))
+            string filePath = @"E:\SHARPCODERS\PROJETOS SHARPCODERS\HUBGAMES\Data\user.json";
+            if (File.Exists(filePath))
             {
-                string jsonLer = File.ReadAllText("user.json");
+                string jsonLer = File.ReadAllText(filePath);
                 PlayerList = JsonConvert.DeserializeObject<List<Player>>(jsonLer);
+                
+
             }
 
         }
--- HUBGAMES/Program.cs
+++ HUBGAMES/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Globalization;
 using HUBGAMES.Model;
 using HUBGAMES.View;
+using System.Text.Json;
+using static HUBGAMES.Model.HubDeGames;
 
 namespace HUBGAMES //aqui nome do projeto sempre
 {
@@ -13,8 +15,21 @@ namespace HUBGAMES //aqui nome do projeto sempre
     {
         public static void Main(string[] args)
         {
+            Players players= new Players();
+
+            var nick = string.Empty;
+            var password = string.Empty;
+            var email = string.Empty;
             
 
+            players.RegisterUser(nick, password, email);
+            // players.Login(password, email);
+
+            //players.DeleteUser(nick, email, password);
+
+            players.ListarTodos();
+
+            //List<Player> listaPlayers
 
 
             Console.ReadKey();
